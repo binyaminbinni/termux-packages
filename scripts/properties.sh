6856fedff9136a8e39203ec610cadc0e595c3bee
Binyamin <124703798+binyaminbinni@users.noreply.github.com>
2026-02-06 09:38:02 -0600

    Add files via upload

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -1848,7 +1848,7 @@ TERMUX_APP__REPO_URL="$TERMUX__REPOS_HOST_ORG_URL/$TERMUX_APP__REPO_NAME"
 #
 # Default value: `com.termux`
 ##
-TERMUX_APP__NAMESPACE="com.bterminal"
+TERMUX_APP__NAMESPACE="com.termux"
 
 __termux_build_props__add_variables_validator_actions "TERMUX_APP__NAMESPACE" "app_package_name"
 
@@ -2180,7 +2180,7 @@ TERMUX_AM_APP__AM_CLASS__CLASS_NAME="$TERMUX_AM_APP__NAMESPACE.Am"
 # and are compiled locally.
 # FIXME: Checking for all variables will be added later in repo
 # changes pull, currently only `TERMUX_REPO_APP__PACKAGE_NAME` is checked.
-TERMUX_REPO_APP__PACKAGE_NAME="com.bterminal"
+TERMUX_REPO_APP__PACKAGE_NAME="com.termux"
 TERMUX_REPO_APP__DATA_DIR="/data/data/com.termux"
 TERMUX_REPO__CORE_DIR="/data/data/com.termux/termux/core"
 TERMUX_REPO__APPS_DIR="/data/data/com.termux/termux/app"
